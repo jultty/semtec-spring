d4bb11b104c8bad8dcb1560c805d5bb23ca2ce64
jultty <juffff@tutanota.de>
2022-02-28 02:04:02 -0300

    configuração inicial do Cucumber
    - Adiciona dependências no arquivo pom.xml
    - Adiciona um arquivo feature com testes em Gherkin
      para as quatro operações CRUD (criar, ler,
      atualizar e apagar).
    - Adiciona um arquivo TermoStepdefs com métodos
      vazios gerados pelo IntelliJ para definir
      etapas de teste.
    - Adiciona o arquivo SemtecApplicationTests.java
      para inicialização de testes

---
+++ src/test/java/TermoStepdefs.java
@@ -0,0 +1,45 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TermoStepdefs {
+    @When("a user sends a POST request")
+    public void aUserSendsAPOSTRequest() {
+    }
+
+    @Then("the server should create and return the item")
+    public void theServerShouldCreateAndReturnTheItem() {
+    }
+
+    @Given("the termo does not already exist")
+    public void theTermoDoesNotAlreadyExist() {
+    }
+
+    @When("a user sends a GET request for a specific ID")
+    public void aUserSendsAGETRequestForASpecificID() {
+    }
+
+    @Then("the server should return the item")
+    public void theServerShouldReturnTheItem() {
+    }
+
+    @Given("it exists in the array")
+    public void itExistsInTheArray() {
+    }
+
+    @When("a user sends a PUT request for a specific ID")
+    public void aUserSendsAPUTRequestForASpecificID() {
+    }
+
+    @Then("the server should update and return the item")
+    public void theServerShouldUpdateAndReturnTheItem() {
+    }
+
+    @When("a user sends a DELETE request for a specific ID")
+    public void aUserSendsADELETERequestForASpecificID() {
+    }
+
+    @Then("the server should delete the item")
+    public void theServerShouldDeleteTheItem() {
+    }
+}
--- src/test/java/com/semtec/api/SemtecApplicationTests.java
+++ src/test/java/com/semtec/api/SemtecApplicationTests.java
@@ -1,13 +1,6 @@
 package com.semtec.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SemtecApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class SemtecApplicationTests { }
